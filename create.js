76808e36ba07db7d0f59a8a7a51a9f3c3d320f0f
John Olson <johnnephiolson@gmail.com>
2024-02-28 11:27:08 -0700

    fixed array creation and storage on create page

--- create.js
+++ create.js
@@ -1,24 +1,29 @@
 //Create array of objects -> new habits (stored locally)
-habitArray = [];
-//When making new habit, push to that array (stringified "JSON.stringify" in local storage)
+habitArray = JSON.parse(localStorage.getItem("arrayOfHabits")) ?? [];
+console.log(habitArray);
+//When making new habit, push to that array (stringified "JSON.stringify" in local storage) JSON.parse
 function addNewHabit() {
-    const newHabit = document.querySelector("#habit");
-    localStorage.setItem("personalHabit", newHabit.value);
-//    window.location.href = "create.html";
-    // Make the order and associate it with a new HTML element
-    const habitElement = document.createElement("li");
-    const myHabit = {element: habitElement, habit: newHabit.value, daysProgress: 0};
-  
-    // Insert the order into the HTML list
-    habitElement.innerHTML = `<span>New Habit: ${newHabit.value}</span>`;
-    const habitList = document.getElementById("habitReview");
-    habitList.appendChild(habitElement);
-  
-    return myHabit;/*
-    const newElement = document.createElement("li");
-    orderElement.innerHTML = <span>Hello is this test working?</span>*/
+    const habitInput = document.querySelector("#habit");
+
+    localStorage.setItem("personalHabit", habitInput.value);
+//  take array from local storage, parse it, then push my newHabit.value onto the array
+    date = "2/28/24"
+    habitArray.push([habitInput.value, date]);
+    localStorage.setItem("arrayOfHabits", JSON.stringify(habitArray));
+    habitInput.value="";
+//  window.location.href = "create.html";
 }
 
-function createOrder() {
-    
+function seeHabits() {
+    habitArray=JSON.parse(localStorage.getItem("arrayOfHabits"));
+    console.log(habitArray);
+    for (let i = 0; i < habitArray.length; i++) {
+        const habitElement = document.createElement("li");
+//  probably useless      const myHabit = {element: habitElement, habit: habitArray[i].value, daysProgress: 0};
+      
+        // Insert the order into the HTML list
+        habitElement.innerHTML = `<span>New Habit: ${habitArray[i][0]} - Date Begun: ${habitArray[i][1]}</span>`;
+        const habitList = document.getElementById("habitReview");
+        habitList.appendChild(habitElement);
+    }
   }
